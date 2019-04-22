db558c8d048ffba98fed763ab49101bfba795e69
James Singleton <jamesrsingleton1@gmail.com>
2019-04-22 00:03:01 -0400

    chore(card): refactor card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,66 +1,44 @@
 import React from 'react';
-import styled, { css, keyframes } from 'styled-components';
-
-const animatedCss = css`
-  opacity: 1;
-  transform: translateY(0);
-`;
-
-const primaryCss = css`
-  background-color: #008bf8;
-  color: #fff;
-`;
+import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
 
 const StyledCard = styled.div`
-  width: ${props => (props.big ? '450px' : '300px')};
-  padding: 15px;
-  opacity: 0;
-  transform: translateY(50px);
-  transition: 500ms all ease-in-out;
-  margin: ${props => (props.noMargin ? 0 : '15px')};
-  box-shadow: 0 5px 15px -5px rgba(0, 0, 0, 1);
-  border-radius: 5px;
-  ${props => props.animated && animatedCss}
-  ${props => props.primary && primaryCss}
+  background: #ffffff;
+  border-radius: 0.5rem;
+  ${props =>
+    props.stretchVertically &&
+    css`
+      height: 100%;
+      flex-grow: 1;
+    `}
+  ${props =>
+    props.hideOverflow &&
+    css`
+      overflow: hidden;
+    `}
 `;
 
-class Card extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      animated: false
-    };
-  }
+const Card = ({ stretchVertically, hideOverflow, className, children }) => (
+  <StyledCard
+    stretchVertically={stretchVertically}
+    hideOverflow={hideOverflow}
+    className={className}
+  >
+    {children}
+  </StyledCard>
+);
 
-  componentDidMount() {
-    const { delay } = this.props;
-    setTimeout(() => {
-      this.setState(() => ({ animated: true }));
-    }, delay);
-  }
+Card.propTypes = {
+  children: PropTypes.node.isRequired,
+  stretchVertically: PropTypes.bool,
+  hideOverflow: PropTypes.bool,
+  className: PropTypes.string
+};
 
-  render() {
-    const {
-      delay = 0,
-      noAnimation,
-      primary,
-      noMargin,
-      big,
-      ...props
-    } = this.props;
-    const { animated } = this.state;
-    return (
-      <StyledCard
-        animated={animated}
-        delay={delay}
-        primary={primary}
-        noAnimation={noAnimation}
-        big={big}
-        noMargin={noMargin}
-        {...props}
-      />
-    );
-  }
-}
+Card.defaultProps = {
+  stretchVertically: false,
+  hideOverflow: true,
+  className: ''
+};
 
 export default Card;
--- stories/card.stories.js
+++ stories/card.stories.js
@@ -6,10 +6,19 @@ import Header from '../src/components/Card/Header'
 
 storiesOf('Card')
   .add('with text', () => (
-    <Card onClick={action('clicked')}>Hello Button</Card>
+    <div style={{ background: '#F0F4F7', padding: 30 }}>
+      <Card onClick={action('clicked')}>Hello Button</Card>
+    </div>
   ))
   .add('with header', () => (
-    <Card>
-      <Header title="Test" />
-    </Card>
+    <div style={{ background: '#F0F4F7', padding: 30 }}>
+      <Card>
+        <Header title="Test" />
+      </Card>
+    </div>
+  ))
+  .add('with custom class name', () => (
+    <div style={{ background: '#F0F4F7', padding: 30 }}>
+      <Card onClick={action('clicked')} className="custom-class">Custom Class Name</Card>
+    </div>
   ))
