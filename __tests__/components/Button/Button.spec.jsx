ad4603342c6ad4e24dddef59784741f5c1737c28
James Singleton <jamesrsingleton1@gmail.com>
2019-04-22 20:00:24 -0400

    test(init): add test for Button

---
+++ __tests__/components/Button/Button.spec.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Button from '../../../src/components/Button';
+
+describe('Button', () => {
+  it('should render as a component', () => {
+    mount(<Button />);
+  });
+});
