798092b7f93f85101d758be71aad8966da6ef308
James Singleton <jamesrsingleton1@gmail.com>
2019-05-06 21:47:31 -0400

    test(theme): add test for theme.js

---
+++ __tests__/theme.spec.js
@@ -0,0 +1,23 @@
+import theme from '../src/theme';
+
+describe('Theme', () => {
+  it('returns color values from theme', () => {
+    const { colors } = theme;
+    const blacks = [
+      'rgba(0,0,0,.0125)',
+      'rgba(0,0,0,.025)',
+      'rgba(0,0,0,.05)',
+      'rgba(0,0,0,.1)',
+      'rgba(0,0,0,.2)',
+      'rgba(0,0,0,.3)',
+      'rgba(0,0,0,.4)',
+      'rgba(0,0,0,.5)',
+      'rgba(0,0,0,.6)',
+      'rgba(0,0,0,.7)',
+      'rgba(0,0,0,.8)',
+      'rgba(0,0,0,.9)',
+    ];
+
+    expect(colors.blacks).toEqual(blacks);
+  });
+});
