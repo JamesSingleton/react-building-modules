5c7f3d4c6a5f760418267bde5e37961fab56b3cd
James Singleton <jamesrsingleton1@gmail.com>
2019-05-08 22:43:57 -0400

    chore(config): switch over to Rollup instead of Webpack

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'cjs',
+  },
+  plugins: [
+    resolve({
+      extensions: ['.mjs', '.js', '.jsx', '.json'],
+      dedupe: ['react', 'react-dom'],
+    }),
+    commonjs({
+      include: 'node_modules/**',
+      // left-hand side can be an absolute path, a path
+      // relative to the current directory, or the name
+      // of a module in node_modules
+      namedExports: {
+        'node_modules/react/index.js': [
+          'cloneElement',
+          'createContext',
+          'Component',
+          'createElement',
+        ],
+        'node_modules/react-dom/index.js': ['render', 'hydrate'],
+        'node_modules/react-is/index.js': ['isElement', 'isValidElementType', 'ForwardRef'],
+      },
+    }),
+    babel({
+      presets: ['amex'],
+      exclude: 'node_modules/**',
+    }),
+  ],
+};
